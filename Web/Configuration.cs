78a95d68a4fe81d4a885636166c52960286d3707
Ansi <the-ansi@yandex.ru>
2015-11-23 23:49:31 +0300

    Web project

---
+++ Web/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Web
+{
+    public static class Configuration
+    {
+        public static double Sin(double alpha)
+        {
+            return Math.Sin(alpha);
+        }
+        public static double Cos(double alpha)
+        {
+            return Math.Cos(alpha);
+        }
+        public static double Min(double first, params double[] args)
+        {
+            return args.Min();
+        }
+        public static double Max(double first, params double[] args)
+        {
+            return Enumerable.Repeat(first, 1).Concat(args).Max();
+        }
+        public static double Sum(double first, params double[] args)
+        {
+            return Enumerable.Repeat(first, 1).Concat(args).Sum();
+        }
+        public static double Pi { get; private set; }
+        static Configuration()
+        {
+            Pi = Math.PI;
+        }
+    }
+}
---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Web.Mvc;
+using AnsiSoft.Calculator.Model.Interface.Facade;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// Clas-contoller
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public IProcessor Processor { get; }
+        /// <summary>
+        /// Create new <see cref="HomeController" /> instance.
+        /// </summary>
+        /// <param name="processorFactory">factory for create processor class.</param>
+        public HomeController(IProcessorFactory processorFactory)
+        {
+            Processor = processorFactory.CreateProcessor();
+        }
+
+        /// <summary>
+        /// Home Page
+        /// </summary>
+        /// <returns>Home Page View</returns>
+        public ActionResult Index() => View();
+
+        /// <summary>
+        /// About Page
+        /// </summary>
+        /// <returns>About Page View</returns>
+        public ActionResult About() => View();
+
+        /// <summary>
+        /// Contact Page
+        /// </summary>
+        /// <returns>Contact Page View</returns>
+        public ActionResult Contact() => View();
+
+        /// <summary>
+        /// Calculate text expression.
+        /// </summary>
+        /// <param name="text">Input text expression</param>
+        /// <returns>Result or error message</returns>
+        public ActionResult Calculate(string text)
+        {
+            string result;
+            try
+            {
+                var value = Processor.Calculate(text);
+                result = value.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (Exception exception)
+            {
+                result = exception.Message;
+            }
+            return Json(new {result}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
