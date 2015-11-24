83970074f44bdfbdc56d3d0b948f7d51f520ef81
Ansi <the-ansi@yandex.ru>
2015-11-24 13:02:37 +0300

    calculator javascript module pattern

--- Web/Scripts/calculator.js
+++ Web/Scripts/calculator.js
@@ -1,49 +1,64 @@
 ﻿/**
- * Action after server response
- * @param {string} text - original expression
- * @param {string} result - result or error message
- * @param {element} processing - calculating control
+ * Calculator class
+ * @param {jQueyy} $ 
+ * @returns {hash} public members
  */
-function writeResult(text, result, processing) {
+function calculator($) {
+    /**
+    * Action after server response
+    * @param {string} text - original expression
+    * @param {string} result - result or error message
+    * @param {element} processing - calculating control
+    */
+    function writeResult(text, result, processing) {
 
-    processing.remove();
-    $("#calculated")
-        .append($("<p></p>").html("Expression: " + text))
-        .append($("<p></p>").html("Result: " + result));
-}
+        processing.remove();
+        $("#calculated")
+            .append($("<p></p>").html("Expression: " + text))
+            .append($("<p></p>").html("Result: " + result));
+    }
 
-/**
- * Send text expression
-  */
-function calculate() {
+    /**
+    * Send text expression
+    */
+    function calculate() {
 
-    var text = $("#expression").val();
-    $("#expression").val("");
+        var text = $("#expression").val();
+        $("#expression").val("");
 
-    var processing = $("<p></p>").html("> " + text);
-    $("#calculating").append(processing);
+        var processing = $("<p></p>").html("> " + text);
+        $("#calculating").append(processing);
 
-    $.ajax({
-        dataType: "json",
-        url: "/Home/Calculate",
-        data: {text: text },
-        success: function(data) {
-            writeResult(text, data.result, processing);
-        },
-        error: function () {
-            writeResult(text, "Server connection error.", processing);
-        }
-    });
-}
+        $.ajax({
+            dataType: "json",
+            url: "/Home/Calculate",
+            data: { text: text },
+            success: function(data) {
+                writeResult(text, data.result, processing);
+            },
+            error: function() {
+                writeResult(text, "Server connection error.", processing);
+            }
+        });
+    }
+
+    /**
+     * Initialization event handlers
+     */
+    function init() {
+        $("#calculate").click(calculate);
+        $("#expression").keypress(function (e) {
+            if (e.which == 13) {
+                calculate();
+            }
+        });
+    }
+    return {init : init}
+};
 
 /**
  * Initialize button press and enter key press
  */
 window.onload = function () {
-    $("#calculate").click(calculate);
-    $("#expression").keypress(function (e) {
-        if (e.which == 13) {
-            calculate();
-        }
-    });
+    calculator(jQuery).init();
 };
